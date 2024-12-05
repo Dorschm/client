5cd7be80eaa9afb8fd581678a194508acd10eb8d
Mark Dorsch <dorschmark@gmail.com>
2024-12-04 23:46:27 -0600

    Update HTML5 build with latest changes including zoom and animation fixes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733376991|707520160';
+const CACHE_VERSION = '1733377546|1263092280';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
