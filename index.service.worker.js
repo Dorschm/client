c0c00c53b443ced905764aefbbd4da8e51559a94
Mark Dorsch <dorschmark@gmail.com>
2024-12-07 12:05:24 -0600

    testing fix 1

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733550056|100726578172';
+const CACHE_VERSION = '1733452973|3647214575';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
