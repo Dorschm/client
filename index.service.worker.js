86658cf9070e0ea22a93cc123d2c260f6318875e
Mark Dorsch <dorschmark@gmail.com>
2024-12-20 19:03:36 -0600

    fixed chat

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1734719617|28689530572';
+const CACHE_VERSION = '1734742988|52060639882';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
