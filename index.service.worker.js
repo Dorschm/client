29487093883b8d2b0f30311cb5a0fb55f2d8d611
Mark Dorsch <dorschmark@gmail.com>
2025-01-07 14:22:23 -0600

    reworked inventory to support context menu

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736275845|86415852557';
+const CACHE_VERSION = '1736281314|91884823925';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
