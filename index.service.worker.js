0a86af7e2b4fba9a9ecca4b379a300a43e41e180
Mark Dorsch <dorschmark@gmail.com>
2025-01-09 16:42:15 -0600

    disconnect sends to login screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736458998|57130419981';
+const CACHE_VERSION = '1736462519|60651268858';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
