34ec3fbee0b7ad7ba5445db0b94a353b2511e70f
Mark Dorsch <dorschmark@gmail.com>
2025-01-08 23:14:19 -0600

    added unequip2

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1736392481|3066642862';
+const CACHE_VERSION = '1736399643|10229352856';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
